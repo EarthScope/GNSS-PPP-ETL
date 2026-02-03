98cb5b8c7403f83f467f8f8905221598020e2436
Franklyn Dunbar <franklyn.dunbar@earthscope.org>
2026-02-02 23:13:43 -0700

    gnss products monolith func

--- dagster_etl/__init__.py
+++ dagster_etl/__init__.py
@@ -3,12 +3,13 @@
 from dagster import Definitions
 
 from .assets import gnss_products, pride_processing
+from gnss_ppp_products.defs.assets import get_date, ftp_product_sources # Import new assets
 from .jobs import daily_gnss_processing_job
 from .resources import gnss_config_resource
 from .schedules import daily_gnss_schedule
 
 defs = Definitions(
-    assets=[*gnss_products.all_assets, *pride_processing.all_assets],
+    assets=[get_date, ftp_product_sources, *gnss_products.all_assets, *pride_processing.all_assets], # Add new assets
     jobs=[daily_gnss_processing_job],
     schedules=[daily_gnss_schedule],
     resources={
