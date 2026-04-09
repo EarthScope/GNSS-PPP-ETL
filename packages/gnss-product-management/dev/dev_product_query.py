603f5b935f7a5ca8e03433db6897fcf1a4be4864
Franklyn Dunbar <franklyn.dunbar@earthscope.org>
2026-04-09 11:22:57 -0600

    adj prod query dev script

--- packages/gnss-product-management/dev/dev_product_query.py
+++ packages/gnss-product-management/dev/dev_product_query.py
@@ -3,8 +3,6 @@ import datetime
 import logging
 
 from gnss_product_management import defaults, ProductQuery
-from gnss_product_management.client import product_query
-from gnss_product_management.environments import workspace
 from gnss_product_management.factories.remote_transport import WormHole
 from gnss_product_management.factories.search_planner import SearchPlanner
 
@@ -24,28 +22,26 @@ date = datetime.datetime(2025, 1, 2, tzinfo=datetime.timezone.utc)
 query_agent = ProductQuery(wormhole=wormhole, search_planner=search_planner).on(date)
 
 
-orbit_agent = (
-    query_agent.for_product("ORBIT").where(TTT="FIN").sources("COD", "ESA").search()
-)
+orbit_agent = query_agent.for_product("ORBIT").where(TTT="FIN").sources("COD", "ESA")
+orbit_results = orbit_agent.search()
 print(f"Search results for ORBIT on {date.date()}:\n")
-for r in orbit_agent:
+for r in orbit_results:
     print(f"  [FOUND]   {r.hostname:<35s}  {r.filename}")
 
-clock_agent = (
-    query_agent.for_product("CLOCK").where(TTT="FIN").sources("COD", "ESA").search()
-)
+clock_agent = query_agent.for_product("CLOCK").where(TTT="FIN").sources("COD", "ESA")
+clock_results = clock_agent.search()
 print(f"\nSearch results for CLOCK on {date.date()}:\n")
-for r in clock_agent:
+for r in clock_results:
     print(f"  [FOUND]   {r.hostname:<35s}  {r.filename}")
 
-erp_agent = (
-    query_agent.for_product("ERP").where(TTT="FIN").sources("COD", "ESA").search()
-)
+erp_agent = query_agent.for_product("ERP").where(TTT="FIN").sources("COD", "ESA")
+erp_results = erp_agent.search()
 print(f"\nSearch results for ERP on {date.date()}:\n")
-for r in erp_agent:
+for r in erp_results:
     print(f"  [FOUND]   {r.hostname:<35s}  {r.filename}")
 
-attobx_agent = query_agent.for_product("ATTOBX").search()
+attobx_agent = query_agent.for_product("ATTOBX")
+attobx_results = attobx_agent.search()
 print(f"\nSearch results for ATTOBX on {date.date()}:\n")
-for r in attobx_agent:
+for r in attobx_results:
     print(f"  [FOUND]   {r.hostname:<35s} {r.directory:<35s} {r.filename}")
